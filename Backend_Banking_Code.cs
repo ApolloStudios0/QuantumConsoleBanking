55c2f11d1dd64eb7f3d96a87739e8b6fd6000897
UT6 <UT6@canicular>
2021-07-28 23:00:18 +0100

    Add project files.

---
+++ Backend_Banking_Code.cs
@@ -0,0 +1,228 @@
+﻿using System;
+using System.Threading;
+
+namespace ConsoleBasedBanking
+{
+    public class Backend_Banking_Code
+    {
+        #region Declarations & Configuration
+        // bool AllowCustomSortCodes = false;
+        // bool AllowCustomAccountNumber = false;
+        // bool DashesInSortCode = false; // Allows for ease of copy-paste
+        // bool FlagOnExceedAllowance = false; // Flag an alert if a user exceeds the daily limit set. (Default $10,000 in one transaction)
+        // bool UploadToDatabase = false; // Link the inputs to a database for re-execution remembrance
+        private double ACCOUNT_NUMBER = 87654321; // Will be randomized later
+        private double ACCOUNT_SORT_CODE = 120034; // Will be randomized later
+        public string AccountOwner = "";
+
+        public string PUBLIC_ACCOUNT_NUMBER { get { return Convert.ToString(ACCOUNT_NUMBER); } } // Public Account Number
+        public string PUBLIC_SORT_CODE { get { return Convert.ToString(ACCOUNT_SORT_CODE); } } // Public Sort Code
+
+        private double _Balance = 1000; // Real Balance Figure
+        public double Balance { get { return _Balance; } } // Public Balance Display
+
+        private double MAX_ALLOWED_LIMIT = 10000; // Default $10,000
+        public bool EXCEEDED_DAILY_LIMIT = false; // Checks if the user has deposited over $10,000 in one transaction.
+        public bool HAS_EXISTING_LOAN;
+
+        public bool Account_Soft_Locked = true; // Soft-lock account used for flagging (Stops spending of large deposits)
+        public bool Account_Hard_Locked = false; // Useful for locking the account entirely. (Suspicious transactions?)
+
+        public string Bank_Contact_Information_Message = ("Please contact the bank @ 0123-234-5678 for more information");
+        public string Emergency_Bank_Contact_Message = ("A serious error has occurred. Please contact the bank immediately @ 0123-234-5678.");
+        #endregion
+
+        public void DepositMoney(double amount)
+        {
+            // Calls : Fraud Checks, Loan Checks, Max Deposit Amounts (Check Config For Options)
+            // Check deposit amount not over $10,000 (flag or alert, check config)
+            if (amount <= 10000 && EXCEEDED_DAILY_LIMIT == false && amount != 0 && Account_Hard_Locked == false)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                _Balance += amount;
+                Console.WriteLine($"You deposited {amount} successfully. Current Balance : {_Balance}");
+                //Console.WriteLine($"{AccountOwner}, Your new balance is {amount}.");
+                //Console.WriteLine($"{amount} Deposited Successfully.");
+                Thread.Sleep(2000);
+                Console.ResetColor();
+            }
+            else if (amount == MAX_ALLOWED_LIMIT) // Equals
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                // *** This puts the transaction through but ghosts (LOCKS) the money so it cant be spent or withdrawn. (Account Locked If Suspicious?)
+                Console.WriteLine($"You deposited {amount} successfully. Current Balance : {_Balance}");
+                Account_Soft_Locked = true; // Soft-Locks the account.
+                Thread.Sleep(2000);
+                Console.ResetColor();
+            }
+            else if (amount >= MAX_ALLOWED_LIMIT) // Greater Than
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"${amount} could not be processed. Please deposit ${MAX_ALLOWED_LIMIT} or less.");
+                Thread.Sleep(2000);
+                Console.ResetColor();
+            }
+            else if (EXCEEDED_DAILY_LIMIT == true)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"Daily deposit amount exceeded. {Bank_Contact_Information_Message}");
+                Thread.Sleep(2000);
+                Console.ResetColor();
+            }
+            else if (amount == 0)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("You cannot deposit $0. Please increase the amount.");
+                Thread.Sleep(2000);
+                Console.ResetColor();
+            }
+            else if (_Balance == 0)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("You cannot withdraw more than your balance. Please edit the amount.");
+                Thread.Sleep(2000);
+                Console.ResetColor();
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(Emergency_Bank_Contact_Message);
+                Thread.Sleep(10000);
+                Console.ResetColor();
+            }
+        } // Deposit Money Method (With Multiple Checks)
+
+        public void WithdrawMoney(double amount)
+        {
+            // Calls : Fraud Checks, Balance Levels, Current Loans, Max Withdraw (Check config for options)
+
+            // Cant withdraw is balance 0
+            // Cant withdraw is account is soft / hard locked
+            // make sure not pulling more than 10,000 then stop transaction and fail it
+
+            if (_Balance != 0 && amount > 0 && amount < 10000 && Account_Hard_Locked == false && Account_Soft_Locked == false)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                _Balance -= amount;
+                Console.WriteLine($"You withdrew {amount}. Your current balance is : {_Balance}");
+                //Console.WriteLine($"{AccountOwner}, you withdrew {amount}. Your current balance is : {_Balance}");
+                //Console.WriteLine($"Withdraw Successful. Current Balance : {_Balance}");
+                Thread.Sleep(2000);
+                Console.ResetColor();
+            }
+            else if (amount >= _Balance)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"You cannot withdraw more than your available balance. {Bank_Contact_Information_Message}");
+                Thread.Sleep(2000);
+                Console.ResetColor();
+            }
+            else if (Account_Soft_Locked == true || Account_Hard_Locked == true)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"There was an unknown error while processing your withdrawal. {Bank_Contact_Information_Message}");
+                Thread.Sleep(2000);
+                Console.ResetColor();
+            }
+            else if (amount == 0)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"You cannot withdraw $0. {Bank_Contact_Information_Message}");
+                Thread.Sleep(2000);
+                Console.ResetColor();
+            }
+            else if (amount >= MAX_ALLOWED_LIMIT) // Greater Than
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"{AccountOwner}, you cannot withdraw more than {MAX_ALLOWED_LIMIT}. {Bank_Contact_Information_Message}");
+                Thread.Sleep(2000);
+                Console.ResetColor();
+            }
+            else if(Balance < 0)
+            {
+                Console.WriteLine(Emergency_Bank_Contact_Message);
+                Thread.Sleep(2000);
+            }
+        } // Withdraw Money Method (With Multiple Checks)
+
+        public void LoanMoney(double amount)
+        {
+            double LOAN_INTEREST_AMOUNT = 9.40; // Default 9.4% APR ( Industry Standard )
+            double TotalLoanAmount = 0;
+            double NEW_LOAN_AMOUNT_TO_PAY = (TotalLoanAmount * LOAN_INTEREST_AMOUNT);
+
+            if (HAS_EXISTING_LOAN == false && Account_Hard_Locked == false && Account_Soft_Locked == false)
+            {
+                Console.Clear();
+                Console.WriteLine($"[ Current Balance : {Balance}]");
+                Console.WriteLine($"[ Account Owner : {AccountOwner}]");
+                Console.WriteLine("-------------------------------");
+                Console.WriteLine("[*] Please select a loan amount:");
+                Console.WriteLine();
+                Console.WriteLine("[1] $100");
+                Console.WriteLine("[2] $250");
+                Console.WriteLine("[3] $500");
+                Console.WriteLine("[4] $1000");
+                Console.WriteLine("[5] Back");
+                Console.WriteLine();
+                Console.Write($"Please enter a choice: ");
+
+                string Loan_Menu_Choice = Console.ReadLine();
+                //double Int_Loan_Menu_Choice = Convert.ToDouble(Loan_Menu_Choice);
+
+                if (Loan_Menu_Choice == "1")
+                {
+                    Console.Clear();
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine($"You took a loan of $100. With {LOAN_INTEREST_AMOUNT}% interest, you must payback {NEW_LOAN_AMOUNT_TO_PAY}");
+                    Console.WriteLine($"");
+                    _Balance += 100; // $100
+                    Console.ResetColor();
+                }
+
+                else if (Loan_Menu_Choice == "2")
+                {
+                    Console.Clear();
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine($"You took a loan of $250. With {LOAN_INTEREST_AMOUNT}% interest, you must payback {NEW_LOAN_AMOUNT_TO_PAY}");
+                    Console.WriteLine($"");
+                    _Balance += 250; // $250
+                    Console.ResetColor();
+                }
+
+                else if (Loan_Menu_Choice == "3")
+                {
+                    Console.Clear();
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine($"You took a loan of $500. With {LOAN_INTEREST_AMOUNT}% interest, you must payback {NEW_LOAN_AMOUNT_TO_PAY}");
+                    Console.WriteLine($"");
+                    _Balance += 500; // $500
+                    Console.ResetColor();
+                }
+
+                else if (Loan_Menu_Choice == "4")
+                {
+                    Console.Clear();
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine($"You took a loan of $1000. With {LOAN_INTEREST_AMOUNT}% interest, you must payback {NEW_LOAN_AMOUNT_TO_PAY}");
+                    Console.WriteLine($"");
+                    _Balance += 1000; // $1000
+                    Console.ResetColor();
+                }
+                else
+                {
+                    Console.Clear();
+                    Console.WriteLine($"Thank you for browsing our loans, {AccountOwner}");
+                    Thread.Sleep(2000);
+                    Console.WriteLine("You can find our offers anytime under the 'Loans' section of the app");
+                    Thread.Sleep(2000);
+                    Console.WriteLine("Returning you to the main screen...");
+                }
+            }
+        } // Loan money (Anything under $10,000 with configurable interest)
+
+        public void Overdraft(double amount) {} // Arrange an overdraft with Quantum Bank (NOT IMPLEMENTED)
+
+        public void BankQuestions() {} // Answer any general questions the user might have (NOT IMPLEMENTED)
+    }
+}
---
+++ Main_CheckingAccount.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Threading;
+using System.Collections.Generic;
+
+namespace ConsoleBasedBanking
+{
+    class CheckingAccount
+    {
+        static void Main()
+        {
+            /* This area is where you can call your functions
+             * Main() is where you can execute all of your commands
+             * But this legacy type Console Based Banking application has pre-built methods
+             * As a reminder, all of this information is falsified and is not real.
+            */
+            #region Variables & Declarations
+            var CheckingAccount = new Backend_Banking_Code();
+            bool Pass_Main_Menu = false;
+            bool NewUser = true;
+            #endregion
+
+            #region Check if the user is new or not
+            while (NewUser)
+            {
+                try
+                {
+                    Console.WriteLine("[ Welcome To The Quantum Banking Application! ]");
+                    Console.WriteLine("[ ------------------------------------------- ]");
+                    Console.Write("[ Please enter your full name: ");
+                    CheckingAccount.AccountOwner = Console.ReadLine();
+                    double Account_Length_String = CheckingAccount.AccountOwner.Length;
+
+                    // Username length checking ( Ensuring the user types in both names, or at least most of it )
+                    if (Account_Length_String < 8)
+                    {
+                        throw new NotImplementedException(); // Loops back with the Try-Catch
+                    }
+
+                    Thread.Sleep(250);
+                    Welcome_The_First_Time_User(); // Running local variables & Configs
+                    NewUser = false; // Makes it so that this wont run again...
+                }
+                catch
+                {
+                    Console.Clear();
+                    Console.WriteLine("Please enter your full name.");
+                    Thread.Sleep(3000);
+                    Console.Clear();
+                }
+            }
+            #endregion
+
+            #region Main Menu Code
+            Console.Clear();
+            while (Pass_Main_Menu == false)
+            {
+                try
+                {
+                    // Console Based Main Menu
+                    Console.Clear();
+                    Console.WriteLine($"[ Current Balance : {CheckingAccount.Balance}]");
+                    Console.WriteLine($"[ Account Owner : {CheckingAccount.AccountOwner}]");
+                    Console.WriteLine("-------------------------------");
+                    Console.WriteLine("[*] Please select an option:");
+                    Console.WriteLine();
+                    Console.WriteLine("[1] Deposit Money");
+                    Console.WriteLine("[2] Withdraw Money");
+                    Console.WriteLine("[3] Loans");
+                    Console.WriteLine("[4] View Settings");
+                    Console.WriteLine("[5] Exit Application");
+                    Console.WriteLine();
+                    Console.Write("Enter an option: ");
+
+                    string form1 = Console.ReadLine();
+                    double MenuChoice = Convert.ToDouble(form1); // Legacy way of doing it..
+
+                    if (MenuChoice == 1)
+                    {
+                        Console.Clear();
+                        Console.WriteLine($"[ Current Balance : {CheckingAccount.Balance} ]");
+                        Console.WriteLine($"[ Account Owner : {CheckingAccount.AccountOwner} ]");
+
+                        Console.Write($"Please enter a deposit amount: ");
+
+                        string Users_Deposit_Amount = Console.ReadLine();
+                        double Int_Users_Deposit_Amount = Convert.ToDouble(Users_Deposit_Amount);
+
+                        Check_The_Transaction();
+
+                        CheckingAccount.DepositMoney(Int_Users_Deposit_Amount);
+                    } // Deposit
+
+                    else if (MenuChoice == 2)
+                    {
+                        Console.Clear();
+                        Console.WriteLine($"[ Current Balance : {CheckingAccount.Balance} ]");
+                        Console.WriteLine($"[ Account Owner : {CheckingAccount.AccountOwner} ]");
+
+                        Console.Write($"Please enter a withdrawal amount: ");
+
+                        string Users_Withdraw_Amount = Console.ReadLine();
+                        double Int_Users_Withdraw_Amount = Convert.ToDouble(Users_Withdraw_Amount);
+
+                        Check_The_Transaction();
+
+                        CheckingAccount.WithdrawMoney(Int_Users_Withdraw_Amount);
+
+                    } // Withdraw
+
+                    else if (MenuChoice == 3)
+                    {
+                        CheckingAccount.LoanMoney(1);
+                        Thread.Sleep(4000);
+                        Console.Clear();
+                    } // Loan
+
+                    else if (MenuChoice == 4)
+                    {
+                        Console.Clear();
+                        Console.WriteLine($"Sorry, {CheckingAccount.AccountOwner} this feature is in development.");
+                        Thread.Sleep(2500);
+                    } // Overdraft
+
+                    else if (MenuChoice == 5)
+                    {
+                        break;
+                    } // Exit
+                }
+
+                catch
+
+                {
+                    Console.WriteLine($"Please enter a valid option.");
+                    Thread.Sleep(3000);
+                    Console.Clear();
+                }
+            }
+            #endregion
+
+            void Welcome_The_First_Time_User()
+            {
+                Console.Clear();
+                Console.WriteLine($"Hello, {CheckingAccount.AccountOwner}. Lets setup your account.");
+                Thread.Sleep(1);
+
+                Console.ForegroundColor = ConsoleColor.Green; // Green for good!
+                Console.WriteLine($"Setting user account level....");
+                //User_Account_Level = "Default"; // (ADMIN, MODERATOR, DEFAULT)
+                Thread.Sleep(1);
+                Console.WriteLine($"Grabbing Global Permissions..."); // For use later
+
+                CheckingAccount.Account_Hard_Locked = false;
+                CheckingAccount.Account_Soft_Locked = false;
+                CheckingAccount.EXCEEDED_DAILY_LIMIT = false;
+
+                Thread.Sleep(1);
+                Console.WriteLine($"Checking Global Connection String...");
+                //CheckingAccount.UploadToDatabase = true;
+                Thread.Sleep(1);
+                Console.WriteLine($"Finishing up.");
+                Thread.Sleep(1);
+                Console.ResetColor();
+            }
+
+            void Check_The_Transaction()
+            {
+                Console.Clear();
+                Console.WriteLine($"Processing your transaction...");
+                Thread.Sleep(1500);
+                Console.Clear();
+                Console.ForegroundColor = ConsoleColor.Green; // Green for good!
+                Console.WriteLine($"Checking Transaction...");
+                Thread.Sleep(1500);
+                Console.ResetColor();
+                Console.Clear();
+            }
+        }
+    }
+}
+
+/* To Do List:
+ * -- Important
+ * Stop withdrawals taking negative number input ( Returns an error saying cannot take more than balance? )
+ * Don't allow balance to go into negatives ( Done by withdrawing more than whats currently in balance )
+ * Finish Loans Menu ( Interest Rate Calculation )
+ * Add Setting Page
+ * Check if overdraft and loans are different... which they basically arent ( Look Online Too )
+ * Check username length ( Anything over 20? )
+ * 
+ * -- To Be Left Last
+ * Speed Up Menu
+ * Cleanse Notes ( Remove Useless Ones / Joke Ones )
+ * Clean Codes ( Mostly Done )
+ * Add admin console ( Remove Users? Add Balance? Remove Balance? Add Loan Amounts? Edit Balance?
+ * 
+ * -- Bank Questions ( Edited on tha fly )
+ * How long did it take to develop Quantum Bank?
+ * What is the purpose of Quantum Console Based Banking?
+ * Who is the developer behind this Quantum Console Bank?
+ * Information about custom loan amounts?
+ * Whats planned for the future?
+ * Will this project be maintained?
+ */
